d6f6f7b01b1b2f0063e420b54a457752c3907d65
unknown <padma5.sai@gmail.com>
2026-01-20 16:13:13 +0530

    changes updated

--- src/main/java/BaseLayer/BaseClass.java
+++ src/main/java/BaseLayer/BaseClass.java
@@ -5,5 +5,10 @@ public class BaseClass {
 	public static void main(String[] args) {
 		System.out.println("Framework is Created for Gitbash");
 	}
+	
+	
+	public static void update() {
+		System.out.println("Framework is Updated for Gitbash");
+	}
 
 }
